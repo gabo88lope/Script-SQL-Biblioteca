668ae473c63c0d29e3d4bcae7c4f6054a3cec3ee
Gabriel Montano <gabo88lope@users.noreply.github.com>
2018-03-04 13:09:25 -0600

    UPDATE2
    corregir errores de sintaxis

--- script.sql
+++ script.sql
@@ -4,26 +4,34 @@
 --Creado sábado, 4 de marzo de 2018
 
 --Tabla Usuario--
-CREATE TABLE Usuario
-(IdUsuario number(5) PRIMARY KEY, 
-Nombre VARCHAR2(20) NOT NULL,
-Apellido VARCHAR2 (20) NOT NULL, 
-Identificacion VARCHAR2 (20) NOT NULL,
-Sexo VARCHAR2(15), 
-Nacionalidad VARCHAR2(20));
+CREATE TABLE Usuario (
+	IdUsuario number(5) PRIMARY KEY, 
+	Nombre VARCHAR2(20) NOT NULL,
+	Apellido VARCHAR2 (20) NOT NULL, 
+	Identificacion VARCHAR2 (20) NOT NULL,
+	Sexo VARCHAR2(15), 
+	Nacionalidad VARCHAR2(20)
+	
+);
 
 --Tabla Ubicación--
-CREATE TABLE Ubicacion
-(IdUbicacion number(3) PRIMARY KEY,
-Nombre VARCHAR2(20) NOT NULL);
+CREATE TABLE Ubicacion (
+	IdUbicacion number(3) PRIMARY KEY,
+	Nombre VARCHAR2(20) NOT NULL)
+	
+);
 
 --Tabla Editorial--
-CREATE TABLE Editorial
-(IdEditorial number(3) PRIMARY KEY,
-Nombre VARCHAR(30) NOT NULL,
-CONSTRAINT FK_Editorial_Ubicacion 
-FOREIGN KEY (IdUbicacion)
-REFERENCES Ubicacion (IdUbicacion));
+CREATE TABLE Editorial (
+	IdEditorial number(3) PRIMARY KEY,
+	Nombre VARCHAR(30) NOT NULL,
+	IdUbicacion NUMBER(3)NOT NULL
+	
+	CONSTRAINT FK_Editorial_Ubicacion 
+	FOREIGN KEY (IdUbicacion)
+	REFERENCES Ubicacion (IdUbicacion)
+
+);
 
 -- TABLA Autor
 CREATE TABLE Autor (
@@ -57,7 +65,7 @@ CREATE TABLE CategoriaEspecial (
     Nombre varchar2(50)  NOT NULL,
     CodigoGeneral number(3)  NOT NULL
 	
-	CONSTRAINT CatEspecial_CatGeneral
+	CONSTRAINT FK_CatEspecial_CatGeneral
     FOREIGN KEY (CodigoGeneral)
     REFERENCES CategoriaGeneral (CodigoGeneral);
     
